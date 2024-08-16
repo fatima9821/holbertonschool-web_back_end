f4d96e9ac154e884178892c08977bff96ce19d82
root <root@FAT>
2024-08-16 21:07:58 +0200

    8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""  type-annotated function make_multiplier """
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_function(value: float) -> float:
+        """ return the value multiplie """
         return value * multiplier
+    """ return multi function """
     return multiplier_function
 
