f104c8406e77bff707c5da0048345c9bd2be3bc5
root <root@FAT>
2024-08-16 20:47:47 +0200

    3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,6 +1,7 @@
-
 #!/usr/bin/env python3
 """ create an asyncio.Task for wait_random function """
+
+
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -9,4 +10,3 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     """ create asyncio.Task for wait_random """
     task = asyncio.create_task(wait_random(max_delay))
     return task
-
