5ce710e9c1c7a4f27f44ae87c58c8a8f8fd102dd
root <root@FAT>
2024-08-23 18:04:07 +0200

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None]:
     """ define a function that genere asynchrone value """
     for i in range(10):
         await asyncio.sleep(1)
