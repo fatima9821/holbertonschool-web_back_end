b3c84f9623dded07c238c438dfd4244c5873293d
root <root@FAT>
2024-08-23 17:56:11 +0200

    m

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Write a coroutine called async_generator """
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ define a function that genere asynchrone value """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" function that write a coroutine called """
+from typing import List
+Vector = List[float]
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Vector:
+    ''' Function that return a list '''
+    Final = [y async for y in async_generator()]
+    return Final
